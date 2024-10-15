46ac0601ea35162095b0d78ae864e782b93a6f18
saibommidi14 <saib19192@gmail.com>
2024-10-15 14:12:38 +0530

    Update Missing.c

--- Missing.c
+++ Missing.c
@@ -1,5 +1,5 @@
 #include"stdio.h"
-void main()
+int main()
 {
     int i,sum=0,miss,add=0;
     int a[15]={1,2,3,4,5,6,7,8,10,11,12,13,14,15};
@@ -10,4 +10,5 @@ void main()
     }
     miss=sum-add;
     printf("%d",miss);
+    return 0;
 }
